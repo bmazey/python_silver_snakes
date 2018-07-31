e19fdf8fe5e947a2b4494023cfa99babad548107
s <shoutianze@outlook.com>
2018-07-31 12:59:13 -0400

    update swear_word_count and add methods in WordList

--- src/WordList.py
+++ src/WordList.py
@@ -1,5 +1,26 @@
-import
+import re
+
 
 class WordList:
 
     def __init__(self):
+        self.list = ['arse', 'ass', 'asshole']
+
+    # returns how many times swear words occur in the text
+    def swear_word_count(self, txt):
+        # count the times of swear words occurring in the text
+        count = 0
+        for word in self.list:
+            if re.search(word, txt):
+                count += 1
+
+        return count
+
+    # add a swear word to the word list
+    def add(self, addition):
+        # test if the list already has the word
+        for word in self.list:
+            if addition.casefold() == word.casefold():
+                print("addition fails")
+            else:
+                self.list.append(addition)
