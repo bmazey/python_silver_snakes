45f64a45797c090b466345ff382e77d5fc01d562
s <shoutianze@outlook.com>
2018-08-01 12:48:47 -0400

    finish up text detection part, ready to launch

--- src/application.py
+++ src/application.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_restplus import Resource, Api
 from src.WordList import *
+from json import *
+from src.Text import *
 
 
 # welcome to flask: http://flask.pocoo.org/
@@ -12,10 +14,27 @@ word_list = WordList()
 # setup the list of swear words
 
 
-@api.route("/wordlist")                   # Create a URL route to this resource
-class WordListOperations(Resource):       # Create a RESTful resource
-    def get(self):                        # Create GET endpoint
-        return {'hello': 'world'}
+@api.route("/word_list")
+class GetWordList(Resource):
+    def get(self):
+        return word_list.generate_swear_word_dict()
+
+
+@api.route("/word_list/<string:word>")
+class WordListOperations(Resource):
+    def get(self, word):
+        return word_list.generate_swear_wod_dict()
+
+    def post(self, word):
+        word_list.add(word)
+        return word_list.generate_swear_word_dict()
+
+
+@api.route("/text_analysis/<string:txt>")
+class WordListOperations(Resource):
+    def post(self, txt):
+        text = Text(txt)
+        return text.generate_analysis()
 
 def main():
     application.debug = True
