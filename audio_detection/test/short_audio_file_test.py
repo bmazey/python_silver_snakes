236b803ad01b848e8d51eaa6276a36467e4b57fc
aj2852 <aj2852@columbia.edu>
2018-08-01 11:08:13 -0400

    google api

--- audio_detection/test/short_audio_file_test.py
+++ audio_detection/test/short_audio_file_test.py
@@ -2,6 +2,6 @@ import unittest
 import audio_detection.src.short_audio_file.py
 
 
-class TestName(unittest.TestCase):
+class Test(unittest.TestCase):
     def test_detect_words(self):
         assert audio_detection.src.short_audio_file.transcribe_file('Its over Anakin! I have the high ground!.mp3') == 'Its over Anakin. I have the high ground.'
